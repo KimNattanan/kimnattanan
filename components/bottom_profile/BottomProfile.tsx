6ee7b32de1e73a88782c5ae9ae9b92bedec54eb2
KimNattanan <jeung.nattanan@gmail.com>
2025-10-14 00:24:17 +0700

    ..

--- components/bottom_profile/BottomProfile.tsx
+++ components/bottom_profile/BottomProfile.tsx
@@ -6,15 +6,15 @@ export default function BottomProfile() {
       <div className="lg:ml-16
                       xs:mr-12
                       mr-6 ml-12">
-        <div className="bg-white border-1 rounded-4xl mx-auto flex flex-col items-center relative
+        {/* <div className="bg-white border-1 rounded-4xl mx-auto flex flex-col items-center relative
                         xl:w-[1000px]
                         lg:w-[800px]
                         md:w-[640px]
                         sm:w-[480px]
                         xs:w-[384px] xs:p-12
-                        w-[248px] px-4 py-8">
+                        w-[248px] px-4 py-8"> */}
           <Project/>
-        </div>
+        {/* </div> */}
       </div>
     </div>
   )
--- components/bottom_profile/Projects.tsx
+++ components/bottom_profile/Projects.tsx
@@ -10,20 +10,26 @@ export default function Project(){
                       text-3xl mb-4">
         {`PROJECTS`}
       </div>
-      <div className='border-1 w-full lg:w-3/4 xl:w-2/3 aspect-[4/3] p-8 mx-auto relative bg-black/5'>
-        <div className='w-full h-full mx-auto relative flex p-10'>
-          <Image
-            src={"/exprec-page3.png"}
-            width={492}
-            height={740}
-            alt="exprec-1"
-          />
-          <Image
-            src={"/exprec-dashboard.jpeg"}
-            width={537}
-            height={1452}
-            alt="exprec-2"
-          />
+      <div className='border-1 w-full lg:w-3/4 xl:w-2/3 aspect-[4/3] p-8 mx-auto relative bg-black/5 my-12'>
+        <div className='flex h-full p-10'>
+          <div className='w-full h-full relative flex overflow-hidden'>
+            <Image
+              src={"/exprec-page3.png"}
+              width={492}
+              height={740}
+              alt="exprec-1"
+              style={{ objectFit: "contain" }}
+            />
+          </div>
+          <div className='h-full relative flex overflow-hidden'>
+            <Image
+              src={"/exprec-dashboard.jpeg"}
+              width={637}
+              height={1254}
+              alt="exprec-2"
+              className=' object-contain'
+            />
+          </div>
         </div>
         <div className='text-xl font-bold text-center'>
           Exprec - Expense Tracking Website
@@ -31,22 +37,45 @@ export default function Project(){
         <div className='text-lg mt-2 mb-8'>
           <div>A simple website for tracking expenses and see summary dashboard. Making the expense noting easier by making templates of price and category.</div>
         </div>
-        <div className='flex justify-center'>
+        <div className='flex justify-center text-sm font-semibold text-cyan-600'>
           <Link href={"https://exprec.kim"} target='_blank'>
-            <div className='text-sm font-semibold w-fit text-cyan-600 border-b-2 cursor-pointer'>
+            <div className='w-fit underline underline-offset-2 cursor-pointer'>
               exprec.kim
             </div>
           </Link>
           <Link href={"https://github.com/KimNattanan/exprec-frontend"} target='_blank'>
-            <div className='text-sm font-semibold w-fit mx-4 text-cyan-600 border-b-2 cursor-pointer'>
+            <div className='w-fit underline underline-offset-2 mx-4 cursor-pointer'>
               github-frontend
             </div>
           </Link>
           <Link href={"https://github.com/KimNattanan/exprec-backend"} target='_blank'>
-            <div className='text-sm font-semibold w-fit text-cyan-600 border-b-2 cursor-pointer'>
+            <div className='w-fit underline underline-offset-2 cursor-pointer'>
               github-backend
             </div>
           </Link>
+        </div>
+      </div>
+      <div className='border-1 w-full lg:w-3/4 xl:w-2/3 p-8 mx-auto relative bg-black/5 my-12'>
+        <div className='text-xl font-bold text-center'>
+          Algorithms and Code Resources in C++
+        </div>
+        <div className='text-lg mt-4 mb-8'>
+          <div className='text-lg font-semibold w-fit text-cyan-600 underline underline-offset-2 cursor-pointer my-2'>
+            <Link href={"https://github.com/KimNattanan/posn"} target='_blank'>
+              Competitive Programming Algorithms and Problem Solutions
+            </Link>
+          </div>
+          <div>
+            Algorithms for competitive programming and solutions to some problems.
+          </div>
+          <div className='text font-semibold w-fit text-cyan-600 underline underline-offset-2 cursor-pointer my-2'>
+            <Link href={"https://github.com/KimNattanan/posn"} target='_blank'>
+              For POSN
+            </Link>
+          </div>
+          <div>
+            More specific scope to POSN.
+          </div>
         </div>
       </div>
     </div>
