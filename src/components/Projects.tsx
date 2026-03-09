2741bd544c60d3be547a1c59cd2b9a584958dc1d
kim <145192439+KimNattanan@users.noreply.github.com>
2026-03-09 16:33:28 +0700

    ..

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -21,153 +21,177 @@ export default function Projects(){
           PROJECTS
         </div>
         <div className="lg:grid lg:grid-cols-2">
-          <div className='relative sm:w-[414px] mx-auto self-center'>
-            <div className='flex py-10 sm:px-10'>
-              <Image
-                src={"/img/projects/exprec-thumb.png"}
-                width={492}
-                height={740}
-                alt="exprec"
-                style={{ objectFit: "contain" }}
-                className='w-full h-full border rounded-xl border-black'
-              />
-            </div>
-            <div className="bg-white py-6 px-8 mb-4 rounded-md border border-stone-200">
-              <div className='text-xl font-bold text-center'>
-                Exprec - Expense Tracking Website
-              </div>
-              <div className='mt-2 text-base'>
-                A simple expense-tracking web application with a summary dashboard. Built with Next.js for the frontend and a Go backend using <span className="text-red2 font-bold">Fiber v2</span> and GORM.
-              </div>
-              <div className="flex mt-4 flex-wrap">
-                <SkillTag title="Next.js"/>
-                <SkillTag title="Typescript"/>
-                <SkillTag title="GO"/>
-                <SkillTag title="Fiber v2"/>
-                <SkillTag title="GORM"/>
-                <SkillTag title="PostgreSQL"/>
-                <SkillTag title="Docker"/>
-                <SkillTag title="AWS"/>
-              </div>
-              <div className='mt-4 flex justify-center text-sm font-semibold text-blue2'>
-                <Link href={"https://github.com/KimNattanan/exprec-backend"} target='_blank'>
-                  <div className='w-fit underline underline-offset-2 cursor-pointer hover:opacity-50'>
-                    Learn more
-                  </div>
-                </Link>
-              </div>
-            </div>
-          </div>
+          <ProjectGoChatBackend/>
           <div className="block lg:hidden sm:mx-12 border-b-2 border-dotted"></div>
-          <div className='relative sm:w-[414px] mx-auto self-center'>
-            <div className='flex p-10'>
-              <Image
-                src={"/img/projects/go-user-service-thumb.png"}
-                width={375}
-                height={375}
-                alt="go-user-service"
-                style={{ objectFit: "contain" }}
-                className='w-full h-full border rounded-full border-black'
-              />
-            </div>
-            <div className="bg-white py-6 px-8 mb-4 rounded-md border border-stone-200">
-              <div className='text-xl font-bold text-center'>
-                Clean Go User Service
-              </div>
-              <div className='mt-2 text-base'>
-                A Go-based user service built with Clean Architecture, featuring Google OAuth2 authentication, PostgreSQL persistence, and secure JWT access and refresh tokens with rotation, exposed via RESTful APIs using <span className="text-red2 font-bold">{`Gorilla Mux`}</span>.
-              </div>
-              <div className="flex mt-4 flex-wrap">
-                <SkillTag title="GO"/>
-                <SkillTag title="Gorilla Mux"/>
-                <SkillTag title="GORM"/>
-                <SkillTag title="PostgreSQL"/>
-                <SkillTag title="Redis"/>
-                <SkillTag title="Docker"/>
-              </div>
-              <div className='mt-4 flex justify-center text-sm font-semibold text-blue2'>
-                <Link href={"https://github.com/KimNattanan/go-user-service"} target='_blank'>
-                  <div className='w-fit underline underline-offset-2 cursor-pointer hover:opacity-50'>
-                    Learn more
-                  </div>
-                </Link>
-              </div>
-            </div>
-          </div>
+          <ProjectExprec/>
           <div className="sm:mx-12 border-b-2 border-dotted"></div>
           <div className="hidden lg:block sm:mx-12 border-b-2 border-dotted"></div>
-          <div className='relative sm:w-[414px] mx-auto self-center'>
-            <div className='flex p-10'>
-              <Image
-                src={"/img/projects/codeforces.webp"}
-                width={256}
-                height={256}
-                alt="codeforces"
-                style={{ objectFit: "contain" }}
-                className='w-full h-full border-black'
-              />
-            </div>
-            <div className="bg-white py-6 px-8 mb-4 rounded-md border border-stone-200">
-              <div className='text-xl font-bold text-center'>
-                Algorithms and Code Resources in C++
-              </div>
-              <div className='mt-2 text-base'>
-                Algorithms for competitive programming and solutions to some problems.
-              </div>
-              <div className="flex mt-4 flex-wrap">
-                <SkillTag title="C++"/>
-              </div>
-              <div className='mt-4 flex justify-center text-sm font-semibold text-blue2'>
-                <Link href={"https://github.com/KimNattanan/compet"} target='_blank'>
-                  <div className='w-fit underline underline-offset-2 cursor-pointer hover:opacity-50'>
-                    Learn more
-                  </div>
-                </Link>
-              </div>
-            </div>
-          </div>
+          <ProjectCleanGoUserService/>
           <div className="block lg:hidden mx-12 border-b-2 border-dotted"></div>
-          <div className='relative sm:w-[414px] mx-auto self-center'>
-            <div className='flex p-10'>
-              <Image
-                src={"/img/projects/go-chat-backend-thumb.png"}
-                width={256}
-                height={256}
-                alt="codeforces"
-                style={{ objectFit: "contain" }}
-                className='w-full h-full border-black'
-              />
-            </div>
-            <div className="bg-white py-6 px-8 mb-4 rounded-md border border-stone-200">
-              <div className='text-xl font-bold text-center'>
-                go-chat-backend
-              </div>
-              <div className='mt-2 text-base'>
-              Chat backend implemented in Go, following a modular monolith architecture with support for REST, gRPC, WebSocket APIs, and RabbitMQ-based messaging.
-              </div>
-              <div className="flex mt-4 flex-wrap">
-                <SkillTag title="in-progress" color="#af7e00"/>
-                <SkillTag title="Go"/>
-                <SkillTag title="Echo"/>
-                <SkillTag title="gRPC"/>
-                <SkillTag title="WebSocket"/>
-                <SkillTag title="RabbitMQ"/>
-                <SkillTag title="Postgres"/>
-                <SkillTag title="GORM"/>
-                <SkillTag title="Redis"/>
-                <SkillTag title="Docker"/>
-              </div>
-              <div className='mt-4 flex justify-center text-sm font-semibold text-blue2'>
-                <Link href={"https://github.com/KimNattanan/go-chat-backend"} target='_blank'>
-                  <div className='w-fit underline underline-offset-2 cursor-pointer hover:opacity-50'>
-                    Learn more
-                  </div>
-                </Link>
-              </div>
-            </div>
-          </div>
+          <ProjectCPPLibrary/>
         </div>
       </div>
     </div>
   )
+}
+
+function ProjectGoChatBackend(){
+  return (
+    <div className='relative sm:w-[414px] mx-auto self-center'>
+      <div className='flex p-10'>
+        <Image
+          src={"/img/projects/go-chat-backend-thumb.png"}
+          width={256}
+          height={256}
+          alt="codeforces"
+          style={{ objectFit: "contain" }}
+          className='w-full h-full border-black'
+        />
+      </div>
+      <div className="bg-white py-6 px-8 mb-4 rounded-md border border-stone-200">
+        <div className='text-xl font-bold text-center'>
+          go-chat-backend
+        </div>
+        <div className='mt-2 text-base'>
+          Chat backend implemented in Go, following a modular monolith architecture with support for REST using <span className="text-red2 font-bold">{`Echo`}</span> framework, gRPC, WebSocket APIs, and RabbitMQ-based messaging.
+        </div>
+        <div className="flex mt-4 flex-wrap">
+          <SkillTag title="in-progress" color="#af7e00"/>
+          <SkillTag title="Go"/>
+          <SkillTag title="Echo"/>
+          <SkillTag title="gRPC"/>
+          <SkillTag title="WebSocket"/>
+          <SkillTag title="RabbitMQ"/>
+          <SkillTag title="Postgres"/>
+          <SkillTag title="GORM"/>
+          <SkillTag title="Redis"/>
+          <SkillTag title="Docker"/>
+        </div>
+        <div className='mt-4 flex justify-center text-sm font-semibold text-blue2'>
+          <Link href={"https://github.com/KimNattanan/go-chat-backend"} target='_blank'>
+            <div className='w-fit underline underline-offset-2 cursor-pointer hover:opacity-50'>
+              Learn more
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function ProjectCleanGoUserService(){
+  return (
+    <div className='relative sm:w-[414px] mx-auto self-center'>
+      <div className='flex p-10'>
+        <Image
+          src={"/img/projects/go-user-service-thumb.png"}
+          width={375}
+          height={375}
+          alt="go-user-service"
+          style={{ objectFit: "contain" }}
+          className='w-full h-full border rounded-full border-black'
+        />
+      </div>
+      <div className="bg-white py-6 px-8 mb-4 rounded-md border border-stone-200">
+        <div className='text-xl font-bold text-center'>
+          Clean Go User Service
+        </div>
+        <div className='mt-2 text-base'>
+          A Go-based user service built with Clean Architecture, featuring Google OAuth2 authentication, PostgreSQL persistence, and secure JWT access and refresh tokens with rotation, exposed via RESTful APIs using <span className="text-red2 font-bold">{`Gorilla Mux`}</span>.
+        </div>
+        <div className="flex mt-4 flex-wrap">
+          <SkillTag title="GO"/>
+          <SkillTag title="Gorilla Mux"/>
+          <SkillTag title="GORM"/>
+          <SkillTag title="PostgreSQL"/>
+          <SkillTag title="Redis"/>
+          <SkillTag title="Docker"/>
+        </div>
+        <div className='mt-4 flex justify-center text-sm font-semibold text-blue2'>
+          <Link href={"https://github.com/KimNattanan/go-user-service"} target='_blank'>
+            <div className='w-fit underline underline-offset-2 cursor-pointer hover:opacity-50'>
+              Learn more
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function ProjectExprec(){
+  return (
+    <div className='relative sm:w-[414px] mx-auto self-center'>
+      <div className='flex py-10 sm:px-10'>
+        <Image
+          src={"/img/projects/exprec-thumb.png"}
+          width={492}
+          height={740}
+          alt="exprec"
+          style={{ objectFit: "contain" }}
+          className='w-full h-full border rounded-xl border-black'
+        />
+      </div>
+      <div className="bg-white py-6 px-8 mb-4 rounded-md border border-stone-200">
+        <div className='text-xl font-bold text-center'>
+          Exprec - Expense Tracking Website
+        </div>
+        <div className='mt-2 text-base'>
+          A simple expense-tracking web application with a summary dashboard. Built with Next.js for the frontend and a Go backend using <span className="text-red2 font-bold">Fiber v2</span> and GORM.
+        </div>
+        <div className="flex mt-4 flex-wrap">
+          <SkillTag title="Next.js"/>
+          <SkillTag title="Typescript"/>
+          <SkillTag title="GO"/>
+          <SkillTag title="Fiber v2"/>
+          <SkillTag title="GORM"/>
+          <SkillTag title="PostgreSQL"/>
+          <SkillTag title="Docker"/>
+          <SkillTag title="AWS"/>
+        </div>
+        <div className='mt-4 flex justify-center text-sm font-semibold text-blue2'>
+          <Link href={"https://github.com/KimNattanan/exprec-backend"} target='_blank'>
+            <div className='w-fit underline underline-offset-2 cursor-pointer hover:opacity-50'>
+              Learn more
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function ProjectCPPLibrary(){
+  return (
+    <div className='relative sm:w-[414px] mx-auto self-center'>
+      <div className='flex p-10'>
+        <Image
+          src={"/img/projects/codeforces.webp"}
+          width={256}
+          height={256}
+          alt="codeforces"
+          style={{ objectFit: "contain" }}
+          className='w-full h-full border-black'
+        />
+      </div>
+      <div className="bg-white py-6 px-8 mb-4 rounded-md border border-stone-200">
+        <div className='text-xl font-bold text-center'>
+          Algorithms and Code Resources in C++
+        </div>
+        <div className='mt-2 text-base'>
+          Algorithms for competitive programming and solutions to some problems.
+        </div>
+        <div className="flex mt-4 flex-wrap">
+          <SkillTag title="C++"/>
+        </div>
+        <div className='mt-4 flex justify-center text-sm font-semibold text-blue2'>
+          <Link href={"https://github.com/KimNattanan/compet"} target='_blank'>
+            <div className='w-fit underline underline-offset-2 cursor-pointer hover:opacity-50'>
+              Learn more
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
